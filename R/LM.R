55535bdbe507dd63d7e212b994afb0268abd3f91
qcchenxiaoman <qcchen@umich.edu>
2022-11-10 23:43:50 -0500

    start mypackage.lm

---
+++ R/LM.R
@@ -0,0 +1,130 @@
+#'LM
+#'
+#'Fit linear regression model by given dataset and corresponding interests of covariates
+#'@param formula input the model which need to be fitted
+#'
+#'@param data input the dataset that the fitted model used
+#'
+#'@return the a list of result: call, terms, residuals, coefficients, coefficients_table, fitted.values, sigma, rank,
+#'df.residual, r.squared, adj.r.squared, fstatistic, model
+#'
+#'@examples
+#'LM(Sepal.Width ~ Sepal.Length, data = iris)
+#'LM(Sepal.Width ~ Sepal.Length + Petal.Length, data = iris)
+#'
+#'@export
+#'
+LM = function(formula, data) {
+  #### Remove NAs ####
+  data = na.omit(data)
+
+  #### Get Y ####
+  Y = matrix(data[[all.vars(formula)[1]]])
+
+  ## Check Y type: need to be numeric
+  ## return a warning if not
+  if (typeof(Y) != "double") {
+    return(cat(
+      c(
+        "Warning messages: ",
+        "1: In model.response(mf, 'numeric') :",
+        "using type = 'numeric' with a factor response will be ignored",
+        "2: In Ops.factor(y, z$residuals) : ‘-’ not meaningful for factors"
+      ),
+      sep = '\n'
+    ))
+  }
+
+  #### Get X, n, p ####
+  X = model.matrix(formula, data)
+  n = nrow(X)
+  p = ncol(X)
+
+  #### Estimation: betahat and var(betahat) ####
+  betahat = solve(t(X) %*% X) %*% t(X) %*% Y
+  Yhat = as.vector(X %*% betahat)
+  names(Yhat) <- row.names(data)
+
+  ## residual
+  epsilonhat = as.vector(Y - Yhat)
+  names(epsilonhat) <- row.names(data)
+
+  ## estimated sigma^2
+  sigma_squared = t(epsilonhat) %*% epsilonhat / (n - p)
+
+  ## variance of betahat
+  var_betahat = diag(solve(t(X) %*% X)) * c(sigma_squared)
+
+  ## se of betahat
+  se_betahat = sqrt(var_betahat)
+
+  #### Inference: t statistic and p val for H0: beta=0 ####
+  t_statistic = (betahat / se_betahat)
+  t_p_value = 2 * pt(q = abs(t_statistic),
+                     df = n - p,
+                     lower.tail = FALSE)
+
+  #### Inference: F statistic and p val for H0: beta1=...=0 ####
+  Ybar = mean(Y)
+  SSE = sum((Y - Yhat) ^ 2)
+  SSR = sum((Yhat - Ybar) ^ 2)
+  SSY = SSE + SSR
+  F_stat = (SSR / (p - 1)) / (SSE / (n - p))
+  F_p_value = 1 - pf(F_stat, (p - 1), (n - p))
+
+  #### Result ####
+  LM_coefficients_table = data.frame(
+    "Estimate" = betahat,
+    "Std. Error" = se_betahat,
+    "t value" = t_statistic,
+    "Pr(>|t|)" = t_p_value, check.names = FALSE
+  )
+
+  LM_r.squared = 1 - SSE / SSY
+  LM_adj.r.squared = 1 - (SSE / (n - p)) / (SSY / (n - 1))
+  LM_model = cbind(data[all.vars(formula)[1]], X[,-1])
+  LM_terms = terms(formula)
+  LM_sigma = sqrt(sigma_squared)[1]
+  LM_fstatistic = cbind(value = F_stat,
+                        numdf = p - 1,
+                        dendf = n - p)
+
+
+  LM_call = paste(c('lm(', formula, ')'), collapse = '')
+  LM_cov.unscaled = solve(t(X) %*% X)
+
+  #### Make result list ####
+  result = list(betahat[, 1],
+                epsilonhat,
+                p,
+                Yhat,
+                n - p,
+                LM_model,
+                LM_call,
+                LM_terms,
+                LM_coefficients_table,
+                LM_sigma,
+                LM_r.squared,
+                LM_adj.r.squared,
+                LM_fstatistic,
+                F_p_value,
+                LM_cov.unscaled)
+
+  names(result) = c("coefficients",
+                    "residuals",
+                    "rank",
+                    "fitted.values",
+                    "df.residual",
+                    "model",
+                    "call",
+                    "terms",
+                    "coefficients_df",
+                    "sigma",
+                    "r.squared",
+                    "adj.r.squared",
+                    "fstatistic",
+                    "F.p-value",
+                    "cov.unscaled")
+  return(invisible(result))
+}
+
---
+++ R/summaryLM.R
@@ -0,0 +1,63 @@
+#'summaryLM
+#'
+#'Mimic summary() of lm()
+#'Print the summary of fitted model in LM()
+#'@param result all output from fitted LM
+#'
+#'@export
+#'
+summaryLM = function(result) {
+  summaryLM_residuals = quantile(result$residuals)
+  names(summaryLM_residuals) <-
+    c("Min", "1Q", "Median", "3Q", "Max")
+
+  LM_pval = result$coefficients_df[, 4]
+  sig_star = ifelse (LM_pval < 0.001, '***',
+                     ifelse (LM_pval < 0.01, '** ',
+                             ifelse(
+                               LM_pval < 0.05, '*  ',
+                               ifelse(LM_pval < 0.1, '.  ', '')
+                             )))
+  LM_pval = ifelse(LM_pval < 2.2e-16, "< 2.2e-16", LM_pval)
+  coefficients_df = result$coefficients_df
+  coefficients_df[, 4] = LM_pval
+  coefficients_table = cbind(coefficients_df, sig_star)
+  names(coefficients_table) = c("Estimate", "Std. Error", "t value", "Pr(>|t|)", "")
+
+  Fpvalue = ifelse(result$`F.p-value` < 2.2e-16, "< 2.2e-16", result$`F.p-value`)
+
+  cat("Call: ", result$call, ' ',  "Residuals: ", sep = '\n')
+  print(summaryLM_residuals)
+
+  cat("\nCoefficients: \n")
+  print(coefficients_table)
+  cat("---\n")
+
+  Signif.codes =
+    "Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1"
+  cat(Signif.codes, "\n\n")
+
+  summary_LM =
+    paste(
+      c(
+        "Residual standard error: ",
+        result$sigma,
+        " on ",
+        result$df.residual,
+        " degrees of freedom \nMultiple R-Squared: ",
+        result$r.squared,
+        ", Adjusted R-squared: ",
+        result$adj.r.squared,
+        "\nF-Statistic: ",
+        result$fstatistic[1],
+        " on ",
+        result$fstatistic[2],
+        " and ",
+        result$fstatistic[3],
+        " DF, p-value: ",
+        Fpvalue
+      ),
+      collapse = ''
+    )
+  cat(summary_LM)
+}
